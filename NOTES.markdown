333deb34d34b6541aaf93776e17f3fa819c4a61f
metastableB <donkdennis@gmail.com>
2016-01-09 11:39:06 +0530

    Model framework for team registration

--- NOTES.markdown
+++ NOTES.markdown
@@ -2,7 +2,24 @@
 
 This repository holds the capture the flag framework used by NJACK, IIT Patna.
 
+Should we allow each individual user to login or should we stick to one team per session login ?
+if inidividual user logges in, should we
 ## TODO :
 [ ] : Modify `user_registration` to incorporate team registration
+[ ] :   Change field names, and error messages
 [ ] : Figure our models structure for team play and points allocation
- 
+[ ] : validate Team name, anwesha ID
+
+## TODO For Registraioint form
+
+User_name -> team name
+user1 : answesha ID
+user2 
+user3 
+College Name
+Phone Number
+Email Id
+
+(Models: Team)
+(Forms: Registration)
+(views: user_rgistration)
--- game_ctf/models.py
+++ game_ctf/models.py
@@ -14,6 +14,17 @@ class Question(models.Model):
 	source_file = models.FilePathField(path=BASE_DIR+"/Questions/")
 	points = models.IntegerField()
 
+class Team(models.Model):
+	# TODO : enforce validators
+	team_name = models.OneToOneField(User,on_delete = models.CASCADE)
+	valid = models.BooleanField(default = True)
+	user1 = models.CharField(max_length = 30)
+	user2 = models.CharField(max_length = 30)
+	user3 = models.CharField(max_length = 30)
+	#college_name = models.EmailField()
+	#phone_number = models.PhoneNumber()
+	points = models.IntegerField(default = 0)
+
 '''
 @metastableB : We are not using a leaderboard specific data structure for this app
 	Our assumption is that the number of users will always remain under, say 1000.
@@ -24,7 +35,3 @@ class Question(models.Model):
 	have to resort to a better LeaderBoard ranking system and scheme, like the ones
 	followed by steam/counter_strike/SO etc
 '''
-class PointsPerUser(models.Model):
-	user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-	points = models.IntegerField(default = 0)
-
