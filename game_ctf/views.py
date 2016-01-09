95cf8153458b0a3ca1e3c35cf43573286ffe2db9
metastableB <donkdennis@gmail.com>
2016-01-09 14:10:26 +0530

    Question Page view returns correct template

--- game_ctf/views.py
+++ game_ctf/views.py
@@ -9,7 +9,7 @@ from django.contrib import messages
 from django.core.urlresolvers import resolve, Resolver404, is_valid_path
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-
+from django.core.exceptions import ObjectDoesNotExist
 from models import Question
 @login_required 
 def home(request):
@@ -18,9 +18,16 @@ def home(request):
 
 
 @login_required
-def question_page(request):
-	# if has context deal with it
-	return HttpResponse("question page")
+def question_page(request,question_id):
+	try:
+		question = Question.objects.get(pk = question_id)
+	except ObjectDoesNotExist:
+		# TODO: WTF ???
+		# return him to home with some funny message
+		pass
+	# TODO :check question status and verify that it is not solved
+	# then go back to home 
+	return render(request,'testQuestions/' + question.source_file)
 
 @login_required 
 def leaderboard(request):
