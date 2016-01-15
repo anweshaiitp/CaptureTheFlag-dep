baa2675d6b13268646d944cf1da1d13c5ffb1f0e
metastableB <donkdennis@gmail.com>
2016-01-16 02:20:31 +0530

    Rules

--- user_session/views.py
+++ user_session/views.py
@@ -111,7 +111,7 @@ def user_registration(request):
 	return render(request, template_path['registration'], {'form' : registration_form})
 
 def email(response):
-	a = send_mail('This is a test', 'Here is the message.', 'ctf@anwesha.info',
+	a = send_mail('Subject', 'Here is the message.', 'ctf@anwesha.info',
     ['asthanasumit23@gmail.com'], fail_silently=False)
 	return HttpResponse("lol")
 	
